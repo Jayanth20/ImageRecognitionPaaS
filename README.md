87b4d4aaa0990ad08c101e8d2129f7028d4d6312
Jayanth20 <jaya.jayanth20@gmail.com>
2022-04-24 12:30:46 -0700

    added code for docker container

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # ImageRecognitionPaaS
- 
+- ["sh","/entry.sh"]
+- pip3 install torch==1.11.0+cpu torchvision==0.11.3+cpu torchaudio==1.10.2+cpu -f https://download.pytorch.org/whl/cpu/torch_stable.html
--- convertVid.py
+++ convertVid.py
@@ -1,12 +1,6 @@
 from picamera import PiCamera
 from time import sleep
-from subprocess import call
-
-import subprocess
-import sys
-
-import os
-
+from subprocess import call 
 
 # Initiate the camera module with pre-defined settings.
 camera = PiCamera()
@@ -14,27 +8,16 @@ camera.resolution = (640, 480)
 camera.framerate = 15
 
 def convert(file_h264, file_mp4):
-    # Record a 5 seconds video.
+    # Record a 15 seconds video.
     camera.start_recording(file_h264)
-    sleep(5)
+    sleep(15)
     camera.stop_recording()
     print("Rasp_Pi => Video Recorded! \r\n")
     # Convert the h264 format to the mp4 format.
     command = "MP4Box -add " + file_h264 + " " + file_mp4
     call([command], shell=True)
     print("\r\nRasp_Pi => Video Converted! \r\n")
-
+    
 
 # Record a video and convert it (MP4).
-convert('/home/pi/Desktop/test.h264', '/home/pi/Desktop/test.mp4')
-
-video_file_path = '/home/pi/Desktop/test.mp4'
-'''
-(ffmpeg
- .input(input_file_name)
- .filter('fps', fps=10, round = 'up')
- .output("%s-%%04d.jpg"%(input_file_name[:-4]), **{'qscale:v': 3})
- .run())'''
-
-path = "/tmp/frames/"
-os.system("ffmpeg -i " + str(video_file_path) + " -r 1 " + str(path) + "image-%3d.jpeg")
+convert('/home/pi/test.h264', '/home/pi/test.mp4')
