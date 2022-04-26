490f0014891fff59113d13101dc2738a9071c83a
SaicharanPapani <90519153+SaicharanPapani@users.noreply.github.com>
2022-04-25 17:48:33 -0700

    Pi code setup is done!

--- convertVid.py
+++ convertVid.py
@@ -28,10 +28,12 @@ import requests
 # Contants
 FILE_UPLOAD_API = 'https://alcozbkh4a.execute-api.us-east-1.amazonaws.com/v1/upload'
 S3_BUCKET_NAME = 'cse546project2videos'
+queue_url = 'https://sqs.us-east-1.amazonaws.com/399730082620/ouputQueue'
 
 # Initiate the camera module with pre-defined settings.
 camera = PiCamera()
-camera.resolution = (640, 480)
+camera.resolution = (160, 160)
+# left, top, right, bottom = 1.5*160, 160 , 2.5*160, 2*160
 camera.framerate = 15
 recordingTimeInsec = 2
 
@@ -41,19 +43,17 @@ sqs = boto3.client("sqs",
     aws_secret_access_key = 'DIXBZuB5utzXQXdYr0yKZAG+MixFa+J1snZYQhDB',
     region_name = 'us-east-1')
 
+# configuring s3
 s3 = boto3.client('s3', aws_access_key_id='AKIAV2EOFAM6NGERWENN',
                       aws_secret_access_key='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')
 
-# queue = sqs.get_queue_by_name(QueueName="outputQueue", region_name="us-east-1")
-
-# make API call for sending images
+# make API call for sending images - vie API gateway
 def uploadImages(location, fileName):
     with open(location, 'rb') as f:
         r = requests.post(FILE_UPLOAD_API, files={'file': f, 'fileName': fileName })
         print(r)
 
-# sendVideos('./face_recognition_docker/hello.png', 'hello.png')
-
+# make API call to send video files to s3
 def uploadVideo(local_file, s3_file):
     try:
         s3.upload_file(local_file, S3_BUCKET_NAME, s3_file)
@@ -63,26 +63,29 @@ def uploadVideo(local_file, s3_file):
     except:
         print("Credentials not available")
 
+# polling messages 
+def poolMessagesFromQueue():
+    while True:
+        print("Polling messages...")
+        response = sqs.receive_message(
+            QueueUrl=queue_url,
+            MaxNumberOfMessages=10,
+            WaitTimeSeconds=1
+        )
+        if 'Messages' in response:
+            for message in response['Messages']:
+                try:
+                    print(message['Body'])
+                except Exception as e:
+                    print(f"exception while processing message: {repr(e)}")
+                    continue
+                receipt_handle = message['ReceiptHandle']
+                sqs.delete_message(
+                    QueueUrl=queue_url,
+                    ReceiptHandle=receipt_handle
+                )
 
-# def monitorSQS():
-
-# this will be excuted as thread for each 0.5 seconds
-# def convertAndUploadFrame(file_h264, file_mp4, i):
-#     # Record a 5 seconds video.
-#     camera.start_recording(file_h264)
-#     sleep(0.5)
-#     camera.stop_recording()
-#     print("Rasp_Pi => Video Recorded! \r\n")
-#     # Convert the h264 format to the mp4 format.
-#     command = "MP4Box -add " + file_h264 + " " + file_mp4
-#     call([command], shell=True)
-#     print("\r\nRasp_Pi => Video Converted! \r\n")
-#     path = "/home/pi/Desktop/CloudProject/frames/"
-#     os.system("ffmpeg -i " + str('/home/pi/Desktop/test'+str(i)+'.mp4') + " -r 1 " + str(path) + "image"+str(i)+"-%3d.jpeg")
-#     t1 = threading.Thread(target=getFaceRecognitionResult, name=str(i))
-#     t1.start()
-#     t1.join()
-    
+# converting and uploading for face recognition
 def getFaceRecognitionResult(fileName, i):
     # coverting to mp4 and storing in video
     file_mp4 ="/home/pi/Desktop/CloudProject/videos/" + str(i) + ".mp4"
@@ -96,18 +99,17 @@ def getFaceRecognitionResult(fileName, i):
     uploadVideo(str('/home/pi/Desktop/CloudProject/videos/'+str(i)+'.mp4'), str(i)+'.mp4')
     # upload image1
     im = Image.open(str(path) + "image"+ str(i)+ "-001.jpeg")
+    # im1 = im.crop((left, top, right, bottom))
     im.save(str(path) + "image"+ str(i)+ "-001.png")
     uploadImages(str(path) + "image"+ str(i)+ "-001.png", "image"+ str(i)+ "-001.png")
     # upload image2
     im = Image.open(str(path) + "image"+ str(i)+ "-002.jpeg")
+    # im1 = im.crop((left, top, right, bottom))
     im.save(str(path) + "image"+ str(i)+ "-002.png")
     uploadImages(str(path) + "image"+ str(i)+ "-002.png", "image"+ str(i)+ "-002.png")
     print("Completed : ", i)
     # uploading file
 
-# # Record a video and convert it (MP4).
-#for i in range(recordingTimeInsec*2):
-#    convertAndUploadFrame('/home/pi/Desktop/CloudProject/videos/test'+str(i)+'.h264', '/home/pi/Desktop/CloudProject/videos/test'+str(i)+'.mp4', i)
 
 threads = []
 camera.start_recording('1.h264')
@@ -116,7 +118,7 @@ sleep(0.5)
 camera.stop_recording()
 threads.append(threading.Thread(target=lambda: getFaceRecognitionResult(str(1)+".h264", 1), name=str(1)))
 threads[-1].start()
-for i in range(2, 4):
+for i in range(2, 3):
     camera.start_recording(str(i)+'.h264')
     print("recording: ", i)
     sleep(0.5)
@@ -124,13 +126,6 @@ for i in range(2, 4):
     threads.append(threading.Thread(target=lambda: getFaceRecognitionResult(str(i)+".h264", i), name=str(i)))
     threads[-1].start()
 
-
-
-# '''
-# (ffmpeg
-#  .input(input_file_name)
-#  .filter('fps', fps=10, round = 'up')
-#  .output("%s-%%04d.jpg"%(input_file_name[:-4]), **{'qscale:v': 3})
-#  .run())'''
-
-
+# polling messages - ran as a separate thread
+threads.append(threading.Thread(target=lambda: poolMessagesFromQueue(), name=str(0)))
+threads[-1].start()
