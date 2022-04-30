f12465a5378d3231f989716186501036b00a4128
SaicharanPapani <90519153+SaicharanPapani@users.noreply.github.com>
2022-04-30 12:33:12 -0700

    Corrections

--- lambdas/UploadVideos.py
+++ lambdas/UploadVideos.py
@@ -11,7 +11,7 @@ def lambda_handler(event, context):
     
     s3 = boto3.client("s3")
     
-    dynamoLambda = boto3.client("lambda")
+    lambdaTrigger = boto3.client("lambda")
 
     # decoding form-data into bytes
     post_data = base64.b64decode(event["body"])
@@ -32,47 +32,67 @@ def lambda_handler(event, context):
 
     # if message is multipart
     if msg.is_multipart():
-        multipart_content = {}
-        # retrieving form-data
-        for part in msg.get_payload():
-            # checking if filename exist as a part of content-disposition header
-            if part.get_filename():
-                # fetching the filename
-                file_name = part.get_filename()
-            multipart_content[
-                part.get_param("name", header="content-disposition")
-            ] = part.get_payload(decode=True)
-
-        # filename from form-data
-        file_name = multipart_content["fileName"]
-        
-        # u uploading file to S3
-        s3_upload = s3.put_object(
-            Bucket=BUCKET_NAME, Key=str(file_name)[2:-1] , Body=multipart_content["file"]
-        )
+        try:
+            multipart_content = {}
+            # retrieving form-data
+            for part in msg.get_payload():
+                # checking if filename exist as a part of content-disposition header
+                if part.get_filename():
+                    # fetching the filename
+                    file_name = part.get_filename()
+                multipart_content[
+                    part.get_param("name", header="content-disposition")
+                ] = part.get_payload(decode=True)
     
-        print("File uploaed successfully")
-        
-        # call facerecognition lambda and get result
-        
-        
-        # temp
-        dynamoSearch = { 'name': 'Saicharan' }
-        
-        dynamoResp = dynamoLambda.invoke(
-            FunctionName = 'arn:aws:lambda:us-east-1:399730082620:function:getResultsFromDynamoDB',
-            InvocationType = 'RequestResponse',
-            Payload = json.dumps(dynamoSearch)
+            # filename from form-data
+            file_name = multipart_content["fileName"]
+            
+            # u uploading file to S3
+            s3_upload = s3.put_object(
+                Bucket=BUCKET_NAME, Key=str(file_name)[2:-1] , Body=multipart_content["file"]
             )
         
-        print(dynamoResp)
-        
-        dynamoRespJson = json.load(dynamoResp['Payload'])
-        
-        print(dynamoRespJson)
-
+            print("File uploaed successfully")
+            
+            # call facerecognition lambda and get result
+            faceRecognitionBody = {
+              "bucketName": BUCKET_NAME,
+              "keyName": str(file_name)[2:-1]
+            }
+            
+            print(faceRecognitionBody)
+            
+            fR_result = lambdaTrigger.invoke(
+                FunctionName = 'arn:aws:lambda:us-east-1:399730082620:function:Face_Recognition',
+                InvocationType = 'RequestResponse',
+                Payload = json.dumps(faceRecognitionBody)
+            )
+            
+            # (hello.png, jayanth_kumar_tumuluri)
+            # print(fR_result['Payload'].read())
+            payload = json.loads(fR_result['Payload'].read())
+            print(payload)
+            studentName = payload[1:].split(",")[1]
+            
+            print(studentName)
+            # # temp
+            dynamoSearch = { 'name': studentName }
+            
+            dynamoResp = lambdaTrigger.invoke(
+                FunctionName = 'arn:aws:lambda:us-east-1:399730082620:function:getResultsFromDynamoDB',
+                InvocationType = 'RequestResponse',
+                Payload = json.dumps(dynamoSearch)
+                )
+                
+            print("Dynamo Response: " , dynamoResp)
+            payload = json.loads(dynamoResp['Payload'].read())
+            if(payload != "Success"):
+                return {"statusCode": 500, "body": json.dumps("Failed to search in DynamoDB!")}
+            
+        except:
+            return {"statusCode": 500, "body": json.dumps("Upload failed or Internal Server Error!")}
         # on upload success
-        return {"statusCode": 200, "body": json.dumps(dynamoRespJson)}
+        return {"statusCode": 200, "body": json.dumps("Sucess!")}
     else:
         # on upload failure
         return {"statusCode": 500, "body": json.dumps("Upload failed!")}
